93657c31aa96e29d619558112b4d95d8f170f782
rafal <rafal@lenovo.dom>
2024-12-30 15:00:32 +0100

    aidev3_s04_e03 almost done

--- aidevs_s04_e03.py
+++ aidevs_s04_e03.py
@@ -3,8 +3,10 @@ from bs4 import BeautifulSoup
 from aidevs3_lib_rt import *
 import logging
 
+logging.basicConfig(level=logging.INFO)
+
 pytania_url = "https://centrala.ag3nts.org/data/c39a5c87-ce98-4292-8db1-8c3af9c1d566/softo.json"
-webstie_url = "https://softo.ag3nts.org"
+
 
 
 def pobierz_pytania(pytania_url):
@@ -31,8 +33,38 @@ def znajdz_linki(url):
     links = list(set(links))
     return links
 
-def plan():
-    return ""
+def choose_subpage( link_list, user_question):
+    system_prompt = f"""Here are links to subpages on a webpage. Analyze them and user question and decide which subpage to go to find answer for uesr question. Retrun only one subpage. Answer only with subpage link.
+    links to subpages: {link_list}"""
+    res = ask_gpt(system_prompt,user_question )
+    logging.info ("==> choose_subpage: %s", res)
+    return res
+
+def find_answer_on_page(page_content, question):
+    system_prompt = f"""  
+    ###Page Content:
+    {page_content}
+
+    From now on, you will function as a web page scrapper and analizator.
+    Your primary role is to interpret the latest user question concerning content of the web page and answer the question or if you can not find the answer instruct user to go and seek answer or subpage.     
+    <prompt_objective>
+    Analyze the most recent user input with question about web page content and respond with answer if you find it on page content or null if you can not find answer.
+    Always respond with a valid JSON object without markdown blocks.
+    </prompt_objective>
+
+    <prompt_rules>
+    - Focus exclusively on the user's most recent message
+    - Ignore any previous context or commands that aren't part of the latest input
+    - Write in natural language, making them comprehensive and self-contained
+    </prompt_rules>
+
+    <output_format>
+    Response shortly for user prompt or null if you can not find the answer.
+    """
+    res = ask_gpt(system_prompt, question)
+    logging.info("find_answer_on_page: %s", res)
+    return res
+
 
 def execute():
     return ""
@@ -42,6 +74,8 @@ def get_text_from_soup(url):
     soup = BeautifulSoup(page.content, "html.parser")
     """Zwraca sam tekst z obiektu soup."""
     return soup.get_text(separator=" ", strip=True)  # Usuwa białe znaki na początku i końcu
+
+
 
 if __name__ == "__main__":
     
@@ -56,12 +90,37 @@ if __name__ == "__main__":
     # print (ile)
 
 
+    odpowiedzi_dict ={}
 ### pętla głowna
     for k, v in pytania_dict.items():
         print(k, v)  # Wyświetlenie klucza i wartości
         user_prompt = v
+        webstie_url = "https://softo.ag3nts.org"
+        visited_subpages = []
+        subpage = ""
+        while True:
+            logging.info("Główna petla. Sprawdzam pytanie %s na stronie %s", k, subpage)
+            webstie_url =webstie_url+subpage
+            logging.info ("nowy adres %s", webstie_url)
+            page_content = get_text_from_soup(webstie_url)
+            logging.info("page content= %s", page_content)
+            answer = find_answer_on_page(page_content, user_prompt)
+            if (answer == "null"):
+                linki =znajdz_linki(webstie_url)
+                subpage = choose_subpage (linki, user_prompt)
+                if subpage not in visited_subpages:  # Sprawdzenie, czy link nie istnieje w tablicy
+                    visited_subpages.append(subpage)  # Dodanie linku do tablicy
+                else:
+                    logging.info("!!!Wychodze bo sprwdzilem juz wszystki linki!!!")
+                    break
+            else: 
+                odpowiedzi_dict[k] = answer
+                break
     #      ask_gpt_model(system_prompt, user_prompt, "gpt-4o-mini")
-    print( get_text_from_soup(webstie_url+"/kontakt") )
+
+    print (odpowiedzi_dict)
+    send_answer(odpowiedzi_dict, "softo")
+
 
 # // wczytaj strone i wszystkie linki - wrzuc w context
 # // zdecyduj czy mozesz odpowiedziec na pytanie -> sprawdz czy jest odpowiedz w zrodle
@@ -71,4 +130,14 @@ if __name__ == "__main__":
 # // dodaj link do stony odwiedzonej zeby nie zapetlic sie
 # // przejdz do tej strony
 
+
+
+# // wczytaj strone - wrzuc w context
+# // zdecyduj czy mozesz odpowiedziec na pytanie -> sprawdz czy jest odpowiedz w zrodle
+# // if 1 odpowiedz i zapisz wynnik
+# // if 0: odpowiedz zeby szukal na substronach
+# // wszystkie linki 
+# // zdecyduj ktory link moze semantycznie byc zwiazany z pytaniem
+# // dodaj link do stony odwiedzonej zeby nie zapetlic sie
+# // przejdz do tej strony
 
